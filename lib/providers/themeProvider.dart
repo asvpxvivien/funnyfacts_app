d80ca6f1334d02a4b0f49a1bcc0ae43a6971e5f7
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-06-06 15:17:03 +0100

    Preserving and loading data

--- lib/providers/themeProvider.dart
+++ lib/providers/themeProvider.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Themeprovider extends ChangeNotifier {
   bool isDarkModeChecked = true;
-  void updateMode({required bool darkMode}) {
+  void updateMode({required bool darkMode}) async {
     isDarkModeChecked = darkMode;
+
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("isDarkModeChecked", darkMode);
+    notifyListeners();
+  }
+
+  void loadMode() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDarkModeChecked = prefs.getBool("isDarkModeChecked") ?? true;
     notifyListeners();
   }
 }
